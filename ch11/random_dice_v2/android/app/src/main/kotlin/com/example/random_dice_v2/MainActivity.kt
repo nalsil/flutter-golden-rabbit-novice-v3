c4853e1fa49cb5731ef18a37e8b5c0950283a2ee
박성진 <sjpark@softgarden.co.kr>
2025-11-04 14:24:01 +0900

    처음 커밋

---
+++ ch11/random_dice_v2/android/app/src/main/kotlin/com/example/random_dice_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.random_dice_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ch12/vid_player_v2/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:vid_player_v2/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomeScreen()));
+}
