fadfb02bc1903975b5a16881384862866d6810ba
박성진 <sjpark@softgarden.co.kr>
2025-11-04 14:24:08 +0900

    처음 커밋

---
+++ ch11/random_dice_v2/lib/screen/settings_screen.dart
@@ -0,0 +1,46 @@
+import 'package:random_dice_v2/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  final double threshold;
+  final ValueChanged<double> onThresholdChanged;
+
+  SettingsScreen({
+    Key? key,
+
+    required this.threshold,
+    required this.onThresholdChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Row(
+            children: [
+              Text(
+                '민감도',
+                style: TextStyle(
+                  color: secondaryColor,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Slider(
+          min: 0.1,
+          max: 10.0,
+          divisions: 101,
+          value: threshold,
+          onChanged: onThresholdChanged,
+          label: threshold.toStringAsFixed(1),
+        ),
+      ],
+    );
+  }
+}
